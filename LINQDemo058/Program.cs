3ca76db290670af07ce2dd82ed13691ebfa1379e
varun <varunlad5@gmail.com>
2021-10-20 22:04:16 +0530

    [Varun] Reyrive Data Based on ProductID and Review

--- LINQDemo058/Program.cs
+++ LINQDemo058/Program.cs
@@ -79,65 +79,6 @@ namespace LINQDemo058
             var data = (list.Where(a => a.Rating > 3 && (a.ProductId == 1 || a.ProductId == 4 || a.ProductId == 9))).ToList();
             Console.WriteLine("The desire Result is :");
             IterateLoopList(data);
-        }
-        //UC4
-        //Counting Each ID present in the List
-        public static void CountingID(List<ProductReview> list)
-        {
-            var data = (list.GroupBy(a => a.ProductId).Select(x => new { ProductId = x.Key, count = x.Count() }));
-            Console.WriteLine("Count of Each Product Id is: ");
-            foreach (var element in data)
-            {
-                Console.WriteLine("Product ID: " + element.ProductId + "\t Count: " + element.count);
-                Console.WriteLine("========================================================");
-            }
-        }
-        //UC5
-        //Retrive only ProductID and Review from the Records
-        public static void ProductIdandReview(List<ProductReview> list)
-        {
-            var p = list.Select(product => new { ProductId = product.ProductId, review = product.Review }).ToList();
-            foreach (var element in p)
-            {
-                Console.WriteLine("Product ID: " + element.ProductId + "\t Review: " + element.review);
-                Console.WriteLine("=====================================");
-            }
-        }
-        //UC6
-        //Skip top 5 Records 
-        public static void SkipTop5Records(List<ProductReview> list)
-        {
-            var result = (from product in list orderby product.Rating descending select product).ToList();
-            Console.WriteLine("After Sorting");
-            IterateLoopList(result);
-            var skipTop5Records = result.Skip(5).ToList();
-            Console.WriteLine("=============================================");
-            Console.WriteLine("Skip Top 5 Records");
-            IterateLoopList(skipTop5Records);
-        }
-        //UC7
-        //Retrive only ProductID and Review from the Records
-        public static void ProductIdandReviewUC7(List<ProductReview> list)
-        {
-            var p = list.Select(product => new { ProductId = product.ProductId, review = product.Review }).ToList();
-            foreach (var element in p)
-            {
-                Console.WriteLine("Product ID: " + element.ProductId + "\t Review: " + element.review);
-                Console.WriteLine("=====================================");
-            }
-        }
-        //UC10
-        //Find Average according to Review
-        public static void ProductIdAverage(List<ProductReview> list)
-        {
-            var data = (list.GroupBy(a => a.ProductId).Select(x => new { ProductId = x.Key, Rating = x.Average() }));
-            //var data = (list.Select(a => a.ProductId).Select(c => new { Rating = c.Average() }));
-            Console.WriteLine("Average of Each Product Id is: ");
-            foreach (var element in data)
-            {
-                Console.WriteLine("Product ID: " + element.ProductId + "\t : " + element.average);
-                Console.WriteLine("========================================================");
-            }
-        }
+        }      
     }
 }
